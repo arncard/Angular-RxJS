0bd481c01b8f52a9319ec784d33967a72c6f880e
rocio.cardenas <rocio.cardenas@gensolve.com>
2020-08-05 13:13:58 +1000

    products list async

--- APM-Start/src/app/products/product-list-alt/product-list-alt.component.ts
+++ APM-Start/src/app/products/product-list-alt/product-list-alt.component.ts
@@ -10,7 +10,7 @@ import { catchError } from 'rxjs/operators';
   selector: 'pm-product-list',
   templateUrl: './product-list-alt.component.html'
 })
-export class ProductListAltComponent implements OnInit, OnDestroy {
+export class ProductListAltComponent implements OnDestroy {
   pageTitle = 'Products';
   errorMessage = '';
   selectedProductId;
@@ -27,13 +27,7 @@ export class ProductListAltComponent implements OnInit, OnDestroy {
   );
 
   constructor(private productService: ProductService) { }
-
-  ngOnInit(): void {
-    this.sub = this.productService.getProducts().subscribe(
-      products => this.products = products,
-      error => this.errorMessage = error
-    );
-  }
+ 
 
   ngOnDestroy(): void {
     this.sub.unsubscribe();
